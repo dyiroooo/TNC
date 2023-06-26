c04af140d367a317f9fc5b3876a8670a88dcd95f
dyiroooo <20200097m.ibia.jiroallen.bscs@gmail.com>
2023-06-26 22:45:07 +0800

    TNC In the making

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+     <v-app-bar app class="teal darken-2 white--text">
+      <v-app-bar-nav-icon class="white--text" @click="toggleSidebar" v-if="showSidebarIcon" />
+      <v-toolbar-title>The New Crossroads</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text class="white--text">Home</v-btn>
+      <v-btn text class="white--text">About</v-btn>
+      <v-btn text class="white--text">Details</v-btn>
+      <v-btn text class="white--text">Contact</v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+     data() {
+    return {
+      showSidebarIcon: true,
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      // Implement the functionality to toggle the sidebar
+      
+    },
+  },
+};
+</script>
